522c86582cacab1991fb2b311432a2b479e3dfa1
Howard Abrams <howard.abrams@workday.com>
2018-01-12 19:10:12 -0800

    Initial chessboard calibration

---
+++ show-corners.py
@@ -0,0 +1,42 @@
+import numpy as np
+import cv2
+import glob
+
+ROWS = 10
+COLS = 13
+HORZ_CORNERS = COLS-1
+VERT_CORNERS = ROWS-1
+
+# termination criteria
+criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+
+# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+objp = np.zeros((HORZ_CORNERS*VERT_CORNERS,3), np.float32)
+objp[:,:2] = np.mgrid[0:VERT_CORNERS,0:HORZ_CORNERS].T.reshape(-1,2)
+
+# Arrays to store object points and image points from all the images.
+objpoints = [] # 3d point in real world space
+imgpoints = [] # 2d points in image plane.
+
+images = glob.glob('*.jpg')
+
+for fname in images:
+    img = cv2.imread(fname)
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+    # Find the chess board corners
+    ret, corners = cv2.findChessboardCorners(gray, (VERT_CORNERS,HORZ_CORNERS),None)
+
+    # If found, add object points, image points (after refining them)
+    if ret == True:
+        objpoints.append(objp)
+
+        corners2 = cv2.cornerSubPix(gray,corners,(11,11),(-1,-1),criteria)
+        imgpoints.append(corners2)
+
+        # Draw and display the corners
+        img = cv2.drawChessboardCorners(img, (VERT_CORNERS,HORZ_CORNERS), corners2,ret)
+        cv2.imshow('img', img)
+        cv2.waitKey(5000)
+
+cv2.destroyAllWindows()
